d09ca6d1a6aef34fb5fe2cf18d80aac094568303
82104 <jonghhhhhh@gmail.com>
2022-01-22 14:49:23 +0900

    1st commit

--- jh_flask/__init__.py
+++ jh_flask/__init__.py
@@ -1,3 +1,10 @@
+''' 터미널에 입력할 실행 코드
+파이썬 터미널: set FLASK_APP=jh_flask
+             set FLASK_ENV=development
+             flask run
+컴퓨터 터미널: ngrok.exe http 5000  (껐다 키면 url 새로 생성)
+'''
+
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -17,7 +24,11 @@ def create_app():
     from . import models
 
     # 블루프린트
-    from .views import main_views
+    from .views import main_views, policynews
     app.register_blueprint(main_views.bp)
+    app.register_blueprint(policynews.bp)
 
     return app
+'''app = create_app()
+if __name__ == '__main__':
+    app.run(debug=True) #host='0.0.0.0', port=4040 등으로 지정 가능'''
